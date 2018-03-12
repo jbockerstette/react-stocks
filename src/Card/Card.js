e88a405ef3648e06b5524954903d83526f9bfe49
jbockerstette <jim.bockerstette@gmail.com>
2018-03-12 13:57:57 -0700

    Looks good but not using css grid

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,14 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
 
-const Card = ({ title, subtitle, detail, actionButton }) => (
+const Card = ({ title, subtitle, detail, actionIcon, actionLabel }) => (
   <div className="card">
     <div className="card__top">
       <div className="card__title">{title}</div>
       <div className="card__subtitle">{subtitle}</div>
       <div className="card__detail">{detail}</div>
     </div>
-    {actionButton}
+    <div className="card__action">
+      <span className="material-icons card__action__icon">{actionIcon}</span>
+      <span className="card__action__label">{actionLabel}</span>
+    </div>
   </div>
 );
 
@@ -17,14 +20,16 @@ Card.defaultProps = {
   title: '',
   subtitle: '',
   detail: '',
-  actionButton: <button>Action</button>
+  actionIcon: <i>add_circle</i>,
+  actionLabel: 'Click me'
 };
 
 Card.propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.string,
   detail: PropTypes.string,
-  actionButton: PropTypes.element
+  actionIcon: PropTypes.element,
+  actionLabel: PropTypes.string
 };
 
 export default Card;

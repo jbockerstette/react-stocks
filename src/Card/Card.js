b75469ba14cc112c15dee375e5c54efe6332f9cb
jbockerstette <jim.bockerstette@gmail.com>
2018-03-15 17:30:08 -0700

    Grid works

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,11 +4,10 @@ import './Card.css';
 
 const Card = ({ title, subtitle, detail, actionIcon, actionLabel }) => (
   <div className="card">
-    <div className="card__top">
-      <div className="card__title">{title}</div>
-      <div className="card__subtitle">{subtitle}</div>
-      <div className="card__detail">{detail}</div>
-    </div>
+    <div className="card__title">{title}</div>
+    <div className="card__subtitle">{subtitle}</div>
+    <div className="card__detail">{detail}</div>
+    <div className="card__divider" />
     <div className="card__action">
       <span className="material-icons card__action__icon">{actionIcon}</span>
       <span className="card__action__label">{actionLabel}</span>
